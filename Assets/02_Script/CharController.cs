7dc5359fd346786945e78f205496ee465daa67b2
JHCircleBell <132878396+JHCircleBell@users.noreply.github.com>
2023-09-05 19:50:35 +0900

    20230905_작업내용
    01. 캐릭터 마우스로 이동
    02. 애니메이션 루트모션 없는걸로 다시 다운 받아와야함

--- Assets/02_Script/CharController.cs
+++ Assets/02_Script/CharController.cs
@@ -6,15 +6,96 @@ public class CharController : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 5f;
-    private Vector3 move = Vector3.zero;
+    private Vector3 move;
+    private Vector3 dir;
+
+    private CharacterController characterController; // 캐릭터 컨트롤러
+    private Camera mainCamera;
+
+    private Animator anim;
+    private static int animParam_Move = Animator.StringToHash("Player_Move");
+    private static int animParam_Run = Animator.StringToHash("Player_Run");
+    private static int animParam_Die = Animator.StringToHash("Player_Die");
+    private static int animParam_Attack01 = Animator.StringToHash("Player_SwordAttack01"); // 우선 일반공격
+
+
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        //if (!TryGetComponent<Camera>(out mainCamera))
+        //{
+        //    Debug.Log("CharController.cs - Awake() - mainCamera 참조 실패");
+        //}
+        
+        if(!TryGetComponent<CharacterController>(out characterController))
+        {
+            Debug.Log("CharController.cs - Awake() - characterController 참조 실패");
+        }
+
+        InitCharController();
+
+    }
+
+    private void InitCharController()
+    {
+        if(!TryGetComponent<Animator>(out anim))
+        {
+            Debug.Log("CharController.cs - Awake() - anim 참조 실패");
+        }
+    }
+
 
     private void Update()
     {
-        move.x = Input.GetAxisRaw("Horizontal");
-        move.z = Input.GetAxisRaw("Vertical");
-        move.Normalize();
+        // 01. 키보드로 캐릭터 이동
+        //move.x = Input.GetAxisRaw("Horizontal");
+        //move.z = Input.GetAxisRaw("Vertical");
+        //move.Normalize();
 
-        transform.position += (moveSpeed * Time.deltaTime * move);
-        transform.LookAt(transform.position + move);
+        //transform.position += (moveSpeed * Time.deltaTime * move);
+        //transform.LookAt(transform.position + move);
+
+
+
+        // 02. 마우스로 캐릭터 이동
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 100f))
+            {
+                move = raycastHit.point;
+
+                Debug.Log("이동 위치 : " + move.ToString());
+            }
+        }
+
+        //if (Vector3.Distance(transform.position, move) > 0.1f)
+        //{
+        //    Move();
+        //}
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetTrigger(animParam_Attack01);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.LeftShift)) // todo.. Run에대한 Animation 다시 찾아오기
+        //{
+        //    anim.SetTrigger(animParam_Run);
+        //}
+
     }
+    private void Move()
+    {
+        
+        transform.position = (move - transform.position).normalized * moveSpeed;
+        
+            
+        characterController.SimpleMove(transform.position);
+    }
+
+    // 참고 코드 : https://stickode.tistory.com/309
 }
