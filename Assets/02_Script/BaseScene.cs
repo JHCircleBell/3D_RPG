dd59000339ad17b640b62ca23192114316a44552
JHCircleBell <tdapfhd3@gmail.com>
2023-10-10 14:55:37 +0900

    do

---
+++ Assets/02_Script/BaseScene.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class BaseScene : MonoBehaviour
+{
+	public Define.Scene SceneType { get; protected set; } = Define.Scene.Unknown;
+	public string nextScene;
+
+	void Awake()
+	{
+		Init();
+	}
+
+	protected virtual void Init()
+	{
+		Object obj = GameObject.FindObjectOfType(typeof(EventSystem));
+		if (obj == null)
+			Managers.Resource.Instantiate("UI/EventSystem").name = "@EventSystem";
+
+		GameObject cam = GameObject.Find("@Rig");
+		if (cam == null)
+		{
+			cam = new GameObject { name = "@Rig" };
+			cam.AddComponent<CameraController>();
+			Camera.main.transform.parent = cam.transform;
+		}
+	}
+
+	public abstract void Clear();
+}
---
+++ Assets/02_Script/Define.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+	public enum WorldObject
+	{
+		Unknown,
+		Player,
+		Enemy
+	}
+
+	public enum State
+	{
+		Idle,
+		Moving,
+		Run,
+		Attack,
+		Skill,
+		Defence,
+		Die,
+	}
+
+	public enum Layer
+	{
+		Enemy = 8,
+		Block = 9,
+		Npc = 10,
+		Obj = 11,
+	}
+
+	public enum Scene
+	{
+		Unknown,
+		TitleScene,
+		LoadingScene,
+		FieldScene,
+		DungeonScene,
+		TestScene,
+	}
+
+	public enum Sound
+	{
+		Bgm,
+		Effect,
+		Enemy,
+		MaxCount,
+	}
+
+	public enum UIEvent
+	{
+		Click,
+		Drag,
+		PointerDown,
+		PointerUp,
+	}
+
+	public enum MouseEvent
+	{
+		Press,
+		PointerDown,
+		PointerUp,
+		Click,
+	}
+
+	public enum CameraMode
+	{
+		QuarterView,
+		QuarterViewManual,
+		EventView,
+	}
+
+	public enum ItemType
+	{
+		Field,
+		Potion,
+	}
+
+	public enum ClickableType
+	{
+		None,
+		Table,
+		PasswordDoor,
+		Vendor,
+	}
+}
